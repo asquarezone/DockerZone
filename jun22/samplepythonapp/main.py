f018877d8b8d7a74d711adbb92794a43b545fe69
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-06-25 20:28:56 +0530

    Added fqdn

--- jun22/samplepythonapp/main.py
+++ jun22/samplepythonapp/main.py
@@ -1,4 +1,3 @@
-
 import flask
 import socket
 
@@ -8,4 +7,5 @@ app = flask.Flask(__name__)
 def index():
     ip_address = flask.request.remote_addr
     server_ip = socket.gethostbyname(socket.gethostname())
-    return f"Requester IP: {ip_address} server ip: {server_ip}"
+    fqdn = socket.getfqdn()
+    return f"Requester IP: {ip_address} server ip: {server_ip} fqdn: {fqdn}"
