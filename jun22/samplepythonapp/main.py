5fcd3f746d53517536e3aa9e4fba3933ed327cb3
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-06-27 07:44:29 +0530

    Added fqdn fix

--- jun22/samplepythonapp/main.py
+++ jun22/samplepythonapp/main.py
@@ -7,8 +7,8 @@ app = flask.Flask(__name__)
 def index():
     ip_address = flask.request.remote_addr
     server_ip = socket.gethostbyname(socket.gethostname())
-    fqdn = socket.getfqdn()
-    return f"Requester IP: {ip_address} server ip: {server_ip} fqdn: {fqdn}"
+    #fqdn = socket.getfqdn()
+    return f"Requester IP: {ip_address} server ip: {server_ip}"
 
 
 app.run(host="0.0.0.0", port=5000)
